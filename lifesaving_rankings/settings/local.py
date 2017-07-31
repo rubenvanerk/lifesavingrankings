5a66c39dd28710d07c6c05cdda8e288f8c86fe80
Ruben van Erk <ruben@rubenvanerk.nl>
2017-07-31 16:16:48 +0200

    production settings

---
+++ lifesaving_rankings/settings/local.py
@@ -0,0 +1,8 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ lifesaving_rankings/settings/production.py
@@ -0,0 +1,11 @@
+from .base import *
+
+ALLOWED_HOSTS = ['www.lifesavingrankings.nl']
+DEBUG = False
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = ''
