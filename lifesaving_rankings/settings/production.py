3fdba17e600261160ba388be9f67afb30a227a57
Ruben van Erk <ruben@rubenvanerk.nl>
2017-08-02 11:21:34 +0200

    default settings file to production

--- lifesaving_rankings/settings/production.py
+++ lifesaving_rankings/settings/production.py
@@ -3,8 +3,9 @@ from .base import *
 ALLOWED_HOSTS = ['www.lifesavingrankings.nl']
 DEBUG = False
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+MEDIA_URL = 'http://91.218.127.89:8000/static/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = ''
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
