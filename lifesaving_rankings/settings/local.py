9b4da02c4afd6490604158370c29413c97082fdb
Ruben van Erk <ruben@rubenvanerk.nl>
2018-05-21 18:27:18 +0200

    enable debug mode in local

--- lifesaving_rankings/settings/local.py
+++ lifesaving_rankings/settings/local.py
@@ -1,9 +1,9 @@
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
