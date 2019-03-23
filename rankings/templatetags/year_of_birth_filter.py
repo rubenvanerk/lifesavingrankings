40e690719a491d6ef9676def0fd7ebf055f95bac
Ruben van Erk <ruben@rubenvanerk.nl>
2019-03-23 12:21:31 +0100

    gender and year of birth displayed in more places

--- rankings/templatetags/year_of_birth_filter.py
+++ rankings/templatetags/year_of_birth_filter.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.filter(name='format_year_of_birth')
 def format_datetime(value):
     if value is None:
-        return ''
+        return ""
     if value < 10:
-        return str(0) + str(value)
-    return value
+        return "'" + str(0) + str(value)
+    return "'" + str(value)
