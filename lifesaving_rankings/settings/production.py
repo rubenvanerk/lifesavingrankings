bfb813d19e6d870b0431f19a00caa2c5d229ec04
rubenvanerk <ruben@rubenvanerk.nl>
2019-12-07 16:44:32 +0100

    Disable aws querystring auth so files get cached by browsers

--- lifesaving_rankings/settings/production.py
+++ lifesaving_rankings/settings/production.py
@@ -12,6 +12,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = env('STATIC_AWS_ID')
 AWS_SECRET_ACCESS_KEY = env('STATIC_AWS_KEY')
 AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME')
+AWS_QUERYSTRING_AUTH = False
 
 # AWS_S3_CUSTOM_DOMAIN = 'rankings-production-static.s3.amazonaws.com'
 # STATIC_URL = 'https://rankings-production-static.s3.eu-central-1.amazonaws.com/javascript/main.js/'
