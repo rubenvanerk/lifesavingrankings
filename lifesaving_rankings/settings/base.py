aa774d1fc029a61fec92485a4f128c3d5500407e
rubenvanerk <ruben@rubenvanerk.nl>
2020-01-11 10:02:15 +0100

    Added .env file for environment variables

--- lifesaving_rankings/settings/base.py
+++ lifesaving_rankings/settings/base.py
@@ -3,6 +3,7 @@ import environ
 
 
 env = environ.Env()
+env.read_env(env.str('ENV_PATH', '.env'))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
