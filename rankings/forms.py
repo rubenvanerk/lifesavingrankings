1ff66a6d0a77ca6e122bed1ab3e6c188eaf95422
Ruben van Erk <ruben@rubenvanerk.nl>
2020-05-07 22:13:35 +0200

    Fixed page for adding results, removed relays from select

--- rankings/forms.py
+++ rankings/forms.py
@@ -9,7 +9,7 @@ class MergeAthletesForm(forms.Form):
 
 
 class AddResultForm(forms.Form):
-    event = forms.ModelChoiceField(queryset=Event.objects.all())  # TODO: filter out relays
+    event = forms.ModelChoiceField(queryset=Event.objects.filter(type__in=[Event.INDIVIDUAL, Event.RELAY_SEGMENT]))
     time = forms.DurationField(label='Time', widget=forms.TextInput(attrs={'placeholder': '00:00.00'}))
     date = forms.DateField(label='Date', widget=forms.TextInput(attrs={'placeholder': 'YYYY-MM-DD'}))
 
