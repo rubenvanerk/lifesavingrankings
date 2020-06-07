0161b335f29546fda3a72aa1741c629d34b9ccff
Ruben van Erk <ruben@rubenvanerk.nl>
2020-06-07 16:24:07 +0200

    Added created_at and updated_at fields to relevant models (#43)

--- analysis/models.py
+++ analysis/models.py
@@ -30,6 +30,8 @@ class AnalysisGroup(models.Model):
     athletes = models.ManyToManyField(Athlete)
     simulation_in_progress = models.BooleanField(default=False)
     simulation_date_from = models.DateField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     class Meta:
         ordering = ('pk',)
