f275bca07c60ec582310d6d737973d622abf0d8a
Ruben van Erk <ruben@rubenvanerk.nl>
2020-06-07 12:27:00 +0200

    Make time nullable

--- rankings/models.py
+++ rankings/models.py
@@ -251,7 +251,7 @@ class IndividualResult(models.Model):
     competition = ForeignKey(Competition, on_delete=models.CASCADE,
                              related_name='individual_results',
                              related_query_name='individual_results')
-    time = models.DurationField()
+    time = models.DurationField(blank=True, null=True)
     points = models.FloatField(default=0)
     original_line = models.CharField(max_length=200, null=True, default=None)
     round = models.IntegerField(default=0)
