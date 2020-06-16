d8e9ffd7d48799704e69af126cf6b77afa73e526
Ruben van Erk <ruben@rubenvanerk.nl>
2020-06-16 21:43:30 +0200

    Fina points formula

--- rankings/functions.py
+++ rankings/functions.py
@@ -15,6 +15,10 @@ def calculate_points(record, seconds):
     return points
 
 
+def calculate_fina_points(record, seconds):
+    return int(1000 * pow(record / seconds, 3))
+
+
 def mk_int(s):
     if s is None:
         return 0
