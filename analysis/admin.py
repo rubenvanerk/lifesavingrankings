a53c3a9ad74326cc100ae0ce8cfe76dd3173a49f
Ruben van Erk <ruben@rubenvanerk.nl>
2021-05-16 13:24:06 +0200

    Add filters to the admin

--- analysis/admin.py
+++ analysis/admin.py
@@ -4,11 +4,13 @@ from analysis.models import *
 
 class AnalysisGroupAdmin(admin.ModelAdmin):
     fields = ['name', 'creator', 'public', 'athletes']
-    list_display = ('name', 'creator', 'public')
+    list_display = ['name', 'creator', 'public']
+    list_filter = ['public', 'creator']
 
 
 class SpecialResultAdmin(admin.ModelAdmin):
-    list_display = ('event', 'time_formatted', 'special_result_group')
+    list_display = ['event', 'time_formatted', 'special_result_group']
+    list_filter = ['special_result_group']
 
 
 admin.site.register(AnalysisGroup, AnalysisGroupAdmin)
--- rankings/admin.py
+++ rankings/admin.py
@@ -4,17 +4,13 @@ from rankings.models import *
 
 class AthleteAdmin(admin.ModelAdmin):
     fields = ['name', 'slug', 'year_of_birth', 'gender', 'nationalities']
-    list_display = ('name', 'year_of_birth', 'gender')
-
-
-class IndividualResultAdmin(admin.ModelAdmin):
-    fields = ['athlete', 'event', ('time', 'reaction_time'), 'competition', ('heat', 'lane', 'round', 'points'),
-              ('disqualified', 'did_not_start', 'withdrawn')]
-    list_display = ('athlete', 'event', 'time_formatted', 'competition')
+    list_display = ['name', 'year_of_birth', 'gender']
+    list_filter = ['gender']
 
 
 class CompetitionAdmin(admin.ModelAdmin):
     list_display = ['name', 'city', 'country', 'date', 'end_date', 'status']
+    list_filter = ['status', 'date']
 
 
 class CountryAdmin(admin.ModelAdmin):
@@ -23,10 +19,19 @@ class CountryAdmin(admin.ModelAdmin):
 
 class EventRecordAdmin(admin.ModelAdmin):
     list_display = ['event', 'gender', 'time_formatted']
+    list_filter = ['gender']
 
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ['name', 'type', 'use_points_in_athlete_total']
+    list_filter = ['type', 'use_points_in_athlete_total']
+
+
+class IndividualResultAdmin(admin.ModelAdmin):
+    fields = ['athlete', 'event', ('time', 'reaction_time'), 'competition', ('heat', 'lane', 'round', 'points'),
+              ('disqualified', 'did_not_start', 'withdrawn')]
+    list_display = ['athlete', 'event', 'time_formatted', 'competition']
+    list_filter = ['event', 'disqualified', 'did_not_start', 'withdrawn']
 
 
 class RelayOrderAdmin(admin.ModelAdmin):
