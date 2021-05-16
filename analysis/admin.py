0e546d3acc017a0456ff11e7dfcdc2c08d6aaac1
Ruben van Erk <ruben@rubenvanerk.nl>
2021-05-16 15:35:07 +0200

    Add search to admin

--- analysis/admin.py
+++ analysis/admin.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from analysis.models import *
 
 
+@admin.register(AnalysisGroup)
 class AnalysisGroupAdmin(admin.ModelAdmin):
     fields = ['name', 'creator', 'public', 'athletes']
     list_display = ['name', 'creator', 'public', 'athlete_count']
     list_filter = ['public', 'creator']
+    search_fields = ['name']
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -17,11 +19,10 @@ class AnalysisGroupAdmin(admin.ModelAdmin):
     athlete_count.admin_order_field = 'athlete_count'
 
 
+@admin.register(SpecialResult)
 class SpecialResultAdmin(admin.ModelAdmin):
     list_display = ['event', 'time_formatted', 'special_result_group']
     list_filter = ['special_result_group']
 
 
-admin.site.register(AnalysisGroup, AnalysisGroupAdmin)
 admin.site.register(SpecialResultGroup)
-admin.site.register(SpecialResult, SpecialResultAdmin)
--- rankings/admin.py
+++ rankings/admin.py
@@ -56,10 +56,12 @@ class CompetitionCountryFilter(admin.SimpleListFilter):
         return queryset
 
 
+@admin.register(Athlete)
 class AthleteAdmin(admin.ModelAdmin):
     fields = ['name', 'slug', 'year_of_birth', 'gender', 'nationalities']
     list_display = ['name', 'year_of_birth', 'gender', 'result_count']
     list_filter = ['gender', EmptyAthleteFilter, AthleteCountryFilter]
+    search_fields = ['name', 'year_of_birth']
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -71,9 +73,11 @@ class AthleteAdmin(admin.ModelAdmin):
     result_count.admin_order_field = 'result_count'
 
 
+@admin.register(Competition)
 class CompetitionAdmin(admin.ModelAdmin):
     list_display = ['name', 'city', 'country', 'date', 'end_date', 'status', 'result_count']
     list_filter = ['status', 'date', CompetitionCountryFilter]
+    search_fields = ['name', 'city', 'country__name']
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -85,8 +89,10 @@ class CompetitionAdmin(admin.ModelAdmin):
     result_count.admin_order_field = 'result_count'
 
 
+@admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     list_display = ['name', 'parent', 'lenex_code', 'athlete_count']
+    search_fields = ['name', 'parent', 'lenex_code']
 
     def get_queryset(self, request):
         qs = super().get_queryset(request)
@@ -98,32 +104,42 @@ class CountryAdmin(admin.ModelAdmin):
     athlete_count.admin_order_field = 'athlete_count'
 
 
+@admin.register(EventRecord)
 class EventRecordAdmin(admin.ModelAdmin):
     list_display = ['event', 'gender', 'time_formatted']
-    list_filter = ['gender']
+    list_filter = ['gender', 'event__type']
 
 
+@admin.register(Event)
 class EventAdmin(admin.ModelAdmin):
     list_display = ['name', 'type', 'use_points_in_athlete_total']
     list_filter = ['type', 'use_points_in_athlete_total']
 
 
+@admin.register(IndividualResult)
 class IndividualResultAdmin(admin.ModelAdmin):
     fields = ['athlete', 'event', ('time', 'reaction_time'), 'competition', ('heat', 'lane', 'round', 'points'),
               ('disqualified', 'did_not_start', 'withdrawn')]
     list_display = ['athlete', 'event', 'time_formatted', 'competition']
     list_filter = ['event', 'disqualified', 'did_not_start', 'withdrawn']
+    search_fields = ['athlete__name', 'competition__name']
 
 
+@admin.register(RelayOrder)
 class RelayOrderAdmin(admin.ModelAdmin):
     list_display = ['segment', 'event', 'index']
 
 
-admin.site.register(Athlete, AthleteAdmin)
-admin.site.register(Competition, CompetitionAdmin)
-admin.site.register(Country, CountryAdmin)
-admin.site.register(EventRecord, EventRecordAdmin)
-admin.site.register(Event, EventAdmin)
-admin.site.register(IndividualResult, IndividualResultAdmin)
-admin.site.register(RelayOrder, RelayOrderAdmin)
-admin.site.register(Team)
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['name', 'participant_count']
+    search_fields = ['name']
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        qs = qs.annotate(participant_count=Count('participation'))
+        return qs
+
+    def participant_count(self, team_instance):
+        return team_instance.participant_count
+    participant_count.admin_order_field = 'participant_count'
